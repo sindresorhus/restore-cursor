70d26cdd1ee9eeb37713d930162e7b4e3899aefe
isaacs <i@izs.me>
2023-09-27 23:28:04 -0700

    Do not print control code to non-terminals (#7)

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import process from 'node:process';
 import onetime from 'onetime';
 import signalExit from 'signal-exit';
 
-const restoreCursor = onetime(() => {
+const terminal = process.stderr.isTTY ? process.stderr :
+	process.stdout.isTTY ? process.stdout :
+		null;
+
+const restoreCursor = terminal ? onetime(() => {
 	signalExit(() => {
-		process.stderr.write('\u001B[?25h');
+		terminal.write('\u001B[?25h');
 	}, {alwaysLast: true});
-});
+}) : () => {};
 
 export default restoreCursor;
