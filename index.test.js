bc322bd50f34cfe9b8926c873e25ae88967ca984
Sindre Sorhus <sindresorhus@gmail.com>
2024-07-26 15:55:32 +0200

    Update dependencies

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 import process from 'node:process';
-import {test} from 'node:test';
+import {test} from 'node:test'; // eslint-disable-line n/no-unsupported-features/node-builtins
 import assert from 'node:assert';
 import {spawnSync} from 'node:child_process';
 import os from 'node:os';
